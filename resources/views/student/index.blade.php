f6cc17fe522c914a00ec57195167a54369f1c0ad
jaydeep-adsum <jaydeep.adsum@gmail.com>
2022-04-25 14:05:27 +0530

    add sample excel file

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -7,7 +7,11 @@
         <div class="col-md-12">
             <div class="card card-default">
                 <div class="card-header">
-                    <div class="card-head-div">
+                    <div class="row">
+                    <div class="col-md-6">
+                        <a href="{{asset('public/sample.xlsx')}}" class="btn btn-primary">Sample Excel File</a>
+                    </div>
+                    <div class="col-md-6 card-head-div">
                         {{ Form::open(['route' => 'import', 'files' => 'true', 'enctype' => 'multipart/form-data']) }}
                         <label class='file-label btn btn-primary mb-0 mr-2'><i class="fa-solid fa-file-excel mr-2"></i>Choose Excel File
                         <input type="file" class="" name="file" accept="" id="import" title="Choose excel file to insert data" onchange="form.submit()">
@@ -16,7 +20,7 @@
                         <a href="{{route('student.create')}}" class="btn btn-primary"><i class="fa-solid fa-plus"></i> Add</a>
                     </div>
                 </div>
-
+                </div>
                 <div class="card-body">
                     @include('student.table')
                 </div>
