6d33006d43c5cf2d163cc49c332d168f69ac66d4
jaydeep-adsum <jaydeep.adsum@gmail.com>
2022-05-02 18:42:11 +0530

    dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,15 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
+use App\Models\Note;
 use App\Models\Student;
+use App\Models\TextBook;
 use Illuminate\Http\Request;
-use App\Models\Room;
 
 class DashboardController extends Controller
 {
     public function dashboard(Request $request)
     {
-        $studentCount = Student::all()->count();
-        return view('admin.home',compact('studentCount'));
+        $data['studentCount'] = Student::all()->count();
+        $data['eventCount'] = Event::all()->count();
+        $data['noteCount'] = Note::all()->count();
+        $data['textBookCount'] = TextBook::all()->count();
+        return view('admin.home', compact('data'));
     }
 }
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,23 +3,56 @@
     Dashboard
 @endsection
 @section('content')
-{{--<div class="row">--}}
-{{--    <div class="col-md-12">--}}
+    {{--<div class="row">--}}
+    {{--    <div class="col-md-12">--}}
     <div class="container">
         <div class="row">
             <div class="col-md-3">
                 <div class="card bg-success">
                     <a href="{{route('student')}}">
-                    <div class="card-body py-4 px-4">
-                        <span style="font-size: 40px;"><i class="fa-solid fa-user-group"></i></span>
-                        <h2 class="fw">{{$studentCount?$studentCount:0}}</h2>
-                        <p>Students</p>
-                    </div>
+                        <div class="card-body py-4 px-4">
+                            <span style="font-size: 40px;"><i class="fa-solid fa-user-group"></i></span>
+                            <h2 class="fw">{{$data['studentCount']?$data['studentCount']:0}}</h2>
+                            <p>Students</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card bg-warning">
+                    <a href="{{route('events')}}">
+                        <div class="card-body py-4 px-4">
+                            <span style="font-size: 40px;"><i class="fa-solid fa-calendar-days"></i></span>
+                            <h2 class="fw">{{$data['eventCount']?$data['eventCount']:0}}</h2>
+                            <p>Events</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card bg-danger">
+                    <a href="{{route('notes')}}">
+                        <div class="card-body py-4 px-4">
+                            <span style="font-size: 40px;"><i class="fa-solid fa-clipboard"></i></span>
+                            <h2 class="fw">{{$data['noteCount']?$data['noteCount']:0}}</h2>
+                            <p>Notes</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card bg-info">
+                    <a href="{{route('textbooks')}}">
+                        <div class="card-body py-4 px-4">
+                            <span style="font-size: 40px;"><i class="fa-solid fa-book-open"></i></span>
+                            <h2 class="fw">{{$data['textBookCount']?$data['textBookCount']:0}}</h2>
+                            <p>Text Books</p>
+                        </div>
                     </a>
                 </div>
             </div>
         </div>
     </div>
-{{--                </div>--}}
-{{--            </div>--}}
+    {{--                </div>--}}
+    {{--            </div>--}}
 @endsection
